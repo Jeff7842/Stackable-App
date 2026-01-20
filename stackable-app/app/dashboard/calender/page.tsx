c5e308523e34b15721a24f17eef9faa1e7e8d317
Jeff7842 <jeffersonkimotho42@gmail.com>
2026-01-20 23:52:27 +0300

    Calender Defualt fix

--- stackable-app/app/dashboard/calender/page.tsx
+++ stackable-app/app/dashboard/calender/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+export default function Page() {
+    return (
+        <div>Calender</div>
+    );
 }
